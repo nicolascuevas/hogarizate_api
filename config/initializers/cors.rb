143a57dc7676c15a04fb15eaa78e536a6c4396df
nicolascuevas <nicolascuevas@gmail.com>
2021-02-16 14:08:23 -0300

    test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,18 +3,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
 
   allow do
-    origins 'hogarizate.cl'
-    resource 'hogarizate.cl',
+    origins '*'
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 
-  allow do
-    origins 'back.hogarizate.cl'
-    resource 'back.hogarizate.cl',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
 
 
 
