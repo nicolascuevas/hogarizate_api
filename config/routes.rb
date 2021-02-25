897dc88911231bed243e1e70a8995a5eb37f7a34
nicolascuevas <nicolascuevas@gmail.com>
2021-02-24 22:19:50 -0300

    root

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations',sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'communes#index'
+  root 'contact_forms#index'
 
   resources :examples, only: :index do
     get :buttons, :cards, :utilities_color, :utilities_border,
