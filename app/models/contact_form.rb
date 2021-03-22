55d0afb3b5c2b5245c6ee97ca3f8f166f7205e57
Nicolas Cuevas <nicolascuevas@gmail.com>
2021-03-22 15:02:32 -0300

    fixes

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -4,6 +4,7 @@ class ContactForm < ApplicationRecord
 	has_many :monthly_incomes
 
 	def save_last_rut_digits
+		self.rut = self.rut.tr(".", "").tr("-","")
 		self.last_digits = self.rut.tr(".", "").tr("-","").last(4).to_s || 0000
 	end
 
