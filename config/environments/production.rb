8a7e23e588a8e9fcfaec203521e0f940f97bfa74
Nicolas Cuevas <nicolascuevas@gmail.com>
2021-03-24 21:47:29 -0300

    CHANGES

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,8 +109,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "cristobal.cerda@hogarizate.cl",
-    :password             => "Hogarizate@2020",
+    :user_name            => ENV['GMAIL_EMAIL'],
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
